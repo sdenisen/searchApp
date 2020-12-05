73481efd22e8cbce5a3d6d1e20f92fa706f20c15
sdenisenko <sdenisen@ymail.com>
2020-12-06 00:19:13 +0300

    added missed files for bootstrap and jquery.

--- searchApp/equipment_parts/views.py
+++ searchApp/equipment_parts/views.py
@@ -11,10 +11,7 @@ def filter(request):
 
 def view_search_results(request):
     _text = request.GET["items"]
-    print(_text)
     list_of_search_query = _text.split(" ")
-    # Details.objects.annotate(full_name_lower=Lower("full_name")).filter(full_name_lower__contains="Подшипник".lower()).values().all()[:5]
-    print(list_of_search_query)
     i_details = Details.objects.annotate(full_name_lower=Lower("full_name"))
     for a in list_of_search_query:
         i_details = i_details.filter(full_name_lower__contains=a)
@@ -22,7 +19,7 @@ def view_search_results(request):
     table_data = i_details.values("asv_id", "full_name")
 
     table_view = DetailsTable(table_data)
-    print(table_view)
+
     print(i_details.query)
     return render(request, "master_page.html", {"table_view": table_view})
 
